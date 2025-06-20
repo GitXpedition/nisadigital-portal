33d9a83144a9fab389c59e2af049391d9e0f950c
GitXpedition <feldman.carlos@gmail.com>
2025-06-19 22:10:22 -0300

    Update i18n.js

--- i18n.js
+++ i18n.js
@@ -16,8 +16,8 @@ i18n
         translation: {
           page_status: "Puesta a Punto del Portal",
           attribution_by: "Por",
-          pillar1_title: "El ruido se desvanece",
-          pillar1_subtitle: "Hasta que solo queda lo importante.",
+          pillar1_title: "Somos Competitivos Para Modernizar su Infraestructura",
+          pillar1_subtitle: "Los procesos de negocio se caracterizan por ser la llave maestra con la que las organizaciones respondenn Nisa Digital eleva en tiempo y forma el reperfilado informatico de su organización. ",
           pillar2_title: "Lo complejo se vuelve simple",
           pillar2_subtitle: "Datos clasificados en su escala correcta aseguran la navegación intuitiva.",
           pillar3_title: "Lo esencial, finalmente visible",
